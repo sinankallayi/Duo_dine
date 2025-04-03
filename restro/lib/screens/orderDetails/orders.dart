3cb9fb421cbdddb222e42d191bfc2edbc2eb26aa
razi <razikallayi@gmail.com>
2025-04-03 19:36:17 +0400

    add order cancelled message

--- restro/lib/screens/orderDetails/orders.dart
+++ restro/lib/screens/orderDetails/orders.dart
@@ -1,6 +1,7 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'package:foodly_ui/constants.dart';
+import 'package:foodly_ui/models/enums/order_status.dart';
 import 'package:foodly_ui/models/order_items_model.dart';
 import 'package:foodly_ui/models/order_model.dart';
 import 'package:foodly_ui/screens/orderDetails/order_timeline/timeline_screen.dart';
@@ -78,8 +79,13 @@ class Orders extends GetView<OrdersController> {
                             ),
                             const SizedBox(height: 4),
                             Text(
-                              "${controller.items[index].status.name.replaceAllMapped(RegExp(r'([a-z])([A-Z])'), (match) => '${match.group(1)} ${match.group(2)}').capitalizeFirst!}",
+                              "${controller.items[index].status.statusText}",
                               style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                            ),
+                            if(controller.items[index].status == OrderStatus.orderCancelled)
+                            Text(
+                              "Your order has been canceled. The refund will be processed to the same account within 7 working days.",
+                              style: TextStyle(fontSize: 14, color: Colors.red[400]),
                             ),
                             const SizedBox(height: 8),
                             Row(
